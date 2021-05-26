8f7e8a87a1ef02bccb3633f895ccbab1f4a75b4b
Nisshaa <nightbbs1@gmail.com>
2021-05-26 15:20:31 +0300

    Подготовка

---
+++ wpf timba/Model/DataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_timba.Model
+{
+    public class DataProvider : IDataProvider
+    {
+        public DataProvider(fileNme: String) { 
+        }
+
+        public IEnumerable<AbiturentNapravlenie> GetAbiturentNapravlenies()
+        {
+            
+        }
+
+        public IEnumerable<Abiturent> GetAbiturents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
