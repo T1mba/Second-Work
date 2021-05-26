5e8a09b5f9c6d21144919319dfa549ec7a7f43c2
Nisshaa <nightbbs1@gmail.com>
2021-05-26 18:35:40 +0300

    Подготовка

--- wpf timba/Model/DataProvider.cs
+++ wpf timba/Model/DataProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,33 @@ namespace wpf_timba.Model
 {
     public class DataProvider : IDataProvider
     {
-        public DataProvider(fileNme: String) { 
+        private List<Abiturent> AbiturientList = new List<Abiturent>();
+
+        public DataProvider(String fileName)
+        {
+            using (TextFieldParser parser = new TextFieldParser(fileName)) {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+
+                while (!parser.EndOfData)
+                {
+                    //метод ReadFields разбивает исходную строку на массив строк
+                    string[] fields = parser.ReadFields();
+                    AbiturientList.Add(
+                        new Abiturent {
+                            Name = fields[0], 
+                            Age = int.Parse(fields[1]),
+                            Klass = int.Parse(fields[2]),
+                            Napravlenie = fields[3],
+                            Ball = Double.Parse(fields[4]),
+                            DataEnd = DateTime.Parse(fields[5])
+                        }
+                        );
+
+                }
+            }
         }
+    
 
         public IEnumerable<AbiturentNapravlenie> GetAbiturentNapravlenies()
         {
@@ -18,7 +44,7 @@ namespace wpf_timba.Model
 
         public IEnumerable<Abiturent> GetAbiturents()
         {
-            throw new NotImplementedException();
+            return AbiturientList;
         }
     }
 }
